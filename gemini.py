ddddd6d6a91e209b5c92a8c2b0f0c280ec650e56
olenevoid <arnafas@gmail.com>
2025-04-13 13:59:47 +0500

    Add image support to gemini
    Response now comes in a dict with text and image content

--- gemini.py
+++ gemini.py
@@ -1,13 +1,47 @@
 from google import genai
+from google.genai import types
 from settings import GEMINI_TOKEN, AI_MODEL
 from md2tgmd import escape
-
-
 from history_manager import HistoryEntry, HistoryManager
+from PIL import Image
+from PIL.ImageFile import ImageFile
+from io import BytesIO
+
+
 client = genai.Client(api_key=GEMINI_TOKEN)
 
-#List of active chats
+# List of active chats
 chats = []
+
+
+def get_generate_content_config():
+    if AI_MODEL.__contains__('exp-image-generation'):
+        config = types.GenerateContentConfig(
+            response_modalities=['Text', 'Image']
+        )
+    else:
+        config = types.GenerateContentConfig(
+            response_modalities=['Text']
+        )
+    return config
+
+
+def get_response_dict(response: types.GenerateContentResponse):
+    text: str = None
+    image: ImageFile = None
+
+    for part in response.candidates[0].content.parts:
+        if part.text:
+            text = escape(part.text)
+        elif part.inline_data:
+            image = Image.open(BytesIO(part.inline_data.data))
+
+    response_dict = {
+        'text': text,
+        'image': image
+    }
+
+    return response_dict
 
 
 def print_models():
@@ -16,38 +50,47 @@ def print_models():
             print(m.name)
 
 
-def request(data: list|str):
-    
-    response = client.models.generate_content(model=AI_MODEL, contents=data)
-    return response.text
+def request(data: list | str) -> dict:
+
+    response = client.models.generate_content(
+        model=AI_MODEL,
+        contents=data,
+        config=get_generate_content_config()
+    )
+
+    return get_response_dict(response)
 
 
-async def request_async(data: list|str):
-    
-    response = await client.aio.models.generate_content(model=AI_MODEL, contents=data)
-    
-    raw_text = response.text
-    text = escape(raw_text)
-    return text
+async def request_async(data: list | str):
+
+    response = await client.aio.models.generate_content(
+        model=AI_MODEL,
+        contents=data,
+        config=get_generate_content_config()
+    )
+
+    return get_response_dict(response)
 
 
 class Chat:
     def __init__(self, chat_id: str, history_manager: HistoryManager):
         self.chat_id = chat_id
-        self.history_manager: HistoryManager = history_manager        
+        self.history_manager: HistoryManager = history_manager
 
+        history = []
         if self.history_manager.entry_exists(chat_id):
-            history = self.history_manager.get_history(chat_id)            
-            self.chat  = client.aio.chats.create(model=AI_MODEL, history=history)
-        else:            
-            self.chat  = client.aio.chats.create(model=AI_MODEL, history=[])
+            history = self.history_manager.get_history(chat_id)
 
-    async def send_message(self, data: list|str):
-        response = await self.chat.send_message(data)                
-        text = escape(response.text)
-        print(f'Gemini response: {text}')
+        self.chat = client.aio.chats.create(
+            model=AI_MODEL,
+            history=history,
+            config=get_generate_content_config()
+        )
+
+    async def send_message(self, data: list | str):
+        response = await self.chat.send_message(data)        
         self.save()
-        return text
+        return get_response_dict(response)['text']
 
     def save(self):
         history_entry = HistoryEntry(self.chat_id, self.chat.get_history())
@@ -72,5 +115,14 @@ def add_new_chat(chat_id: str):
     return new_chat
 
 
+def main():
+    request_text = 'Generate a random image and give a short description of it'
+
+    response = request(request_text)
+    print(response['text'])
+    image: ImageFile = response['image']
+    image.save('response_image.png')
+
+
 if __name__ == '__main__':
-    pass
+    main()
